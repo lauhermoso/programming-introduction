92460c3a719791e28b40cb472df8387d5f5948c3
Lau <106471544+lauhermoso@users.noreply.github.com>
2022-11-23 21:40:21 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int param1 = 10;
+        int param2 = 30;
+
+        var valor =suma(param1, param2);
+
+        System.out.println(valor);
+    }
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+}
+
