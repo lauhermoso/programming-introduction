30a02c712fe60c7e234b0782cbdefaaa9740d47e
Lau <106471544+lauhermoso@users.noreply.github.com>
2022-11-28 18:37:46 +0100

    exercise 2 - programming introduction

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,50 @@
 public class Main {
     public static void main(String[] args) {
-        int param1 = 10;
-        int param2 = 30;
-        int param3 = 10;
+        int numeroif = 4;
 
-        var valor =suma(param1, param2, param3);
+        if (numeroif > 0) {
+            System.out.println("Es positivo");
+        } else if (numeroif < 0) {
+            System.out.println("Es negativo");
+        } else {
+            System.out.println("Es 0");
+        }
+        int numerowhile = 0;
 
-        System.out.println(valor);
+        while (numerowhile < 3) {
+            System.out.println(numerowhile);
+            numerowhile = numerowhile + 1;
+        }
 
-        Coche miCoche = new Coche();
-        miCoche.sumarPuerta();
+        int numerodowhile = 2;
 
-        System.out.println(miCoche.puertas);
-    }
-    public static int suma(int a, int b, int c) {
-        return a + b + c;
-    }
-}
+        do {
+            System.out.println(numerodowhile);
+            numerodowhile = numerodowhile + 1;
+        } while (numerodowhile < 3);
 
-class Coche {
-    public int puertas = 4;
+        for(int numerofor = 0; numerofor <= 3; numerofor = numerofor + 1) {
+            System.out.println (numerofor);
+        }
 
-    public void sumarPuerta() {
-        this.puertas++;
+        var estacion= "otoño";
+
+        switch(estacion) {
+            case "invierno":
+                System.out.println("es invierno");
+                break;
+            case "verano":
+                System.out.println("es verano");
+                break;
+            case "primavera":
+                System.out.println("es primavera");
+                break;
+            case "otoño":
+                System.out.println("es otoño");
+                break;
+            default:
+                System.out.println("estoy en default");
+
+        }
     }
 }
